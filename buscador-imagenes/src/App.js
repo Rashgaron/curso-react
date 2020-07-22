98f3eca306cec9171d28ce9afed5c4732cb5f7e8
Rashgaron <dannirodriguez99@gmail.com>
2020-07-22 21:57:24 +0200

    ddf

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -1,31 +1,83 @@
 import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
   // state de la app
   const [busqueda, guardarBusqueda] = useState("");
-
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaActual, guardarPaginaActual] = useState(1);
+  const [totalPaginas, guardarTotalPaginas] = useState(1);
   useEffect(() => {
     const consultarApi = async () => {
       if (busqueda === "") return;
       else {
         const imagenesPorPagina = 30;
         const key = "17478426-7402ae0bda556fa4a7b847cbf";
-        const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}
+        const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaActual}
     `;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
-        console.log(resultado.hits);
+        guardarImagenes(resultado.hits);
+        // calcular el total de paginas
+        const calcularTotalPaginas = Math.ceil(
+          resultado.totalHits / imagenesPorPagina
+        );
+        guardarTotalPaginas(calcularTotalPaginas);
       }
+      // Mover pantalla hacia arriba
+
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({ behavior: "smooth" });
     };
     consultarApi();
-  }, [busqueda]);
+  }, [busqueda, paginaActual]);
+
+  // definir la página paginaAnterior
+
+  const paginaAnterior = () => {
+    if (paginaActual > 1) {
+      const nuevaPaginaActual = paginaActual - 1;
+
+      guardarPaginaActual(nuevaPaginaActual);
+    }
+  };
+
+  const paginaSiguiente = () => {
+    if (paginaActual < totalPaginas) {
+      const nuevaPaginaActual = paginaActual + 1;
+
+      guardarPaginaActual(nuevaPaginaActual);
+    }
+  };
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
         <Formulario guardarBusqueda={guardarBusqueda}></Formulario>
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes}></ListadoImagenes>
+
+        {paginaActual === 1 ? null : (
+          <button
+            type="button"
+            className="bbtn btn-info mr-1"
+            onClick={paginaAnterior}
+          >
+            &laquo; Anterior{" "}
+          </button>
+        )}
+        {paginaActual === totalPaginas ? null : (
+          <button
+            type="button"
+            className="bbtn btn-info "
+            onClick={paginaSiguiente}
+          >
+            Siguiente &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
---
+++ buscador-musica/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import Formulario from './components/Formulario'
+function App() {
+  return (
+    <Fragment>
+      <Formulario></Formulario>
+    </Fragment>
+  );
+}
+
+export default App;
